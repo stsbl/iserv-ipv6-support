d6db02d45735bc522b9875a747326010da01f6f6
Felix Jacobi <felix.jacobi@iserv.eu>
2017-11-06 22:39:57 +0100

    bind: Added lan internal support for psr recrods

--- iconf/etc/bind/db.forward/95ipv6-support_lan.sh
+++ iconf/etc/bind/db.forward/95ipv6-support_lan.sh
@@ -8,5 +8,12 @@ do
   echo -e "@\t\tAAAA\t$i"
   echo -e "wpad\t\tAAAA\t$i"
   echo -e "$(hostname -s)\t\tAAAA\t$i"
+  echo -e "ipv6\t\tAAAA\t$i"
 done
 
+for i in $(netquery -p ip)
+do
+  echo -e "ipv4\t\tA\t$i"
+done
+
+echo
