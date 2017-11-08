ff94ea4c484e184c0705947b5f055a020fbdcd84
Felix Jacobi <felix.jacobi@iserv.eu>
2017-11-08 03:22:01 +0100

    Adapted iservchk scripts for new DHCPv6 behaviour

--- iservchk/40dhcpd6/20ipv6-support.sh
+++ iservchk/40dhcpd6/20ipv6-support.sh
@@ -9,7 +9,10 @@ echo 'ChPerm 0755 root:root /etc/init.d/isc-dhcp-server6'
 
 if [ "$UseDHCPv6" ] && [ "$DHCP" ] && netquery6 --global --lan --quiet
 then
-  echo 'Touch /var/lib/iserv/config/dhcpd6.conf.hosts'
+  for i in $(netquery6 --global --lan --format nic)
+  do
+    echo 'Touch /var/lib/iserv/config/dhcpd6.conf.'$i'.hosts'
+  done
   echo 'Touch /etc/dhcp/dhcpd6.conf.local'
   echo 'Check /etc/dhcp/dhcpd6.conf'
   echo
