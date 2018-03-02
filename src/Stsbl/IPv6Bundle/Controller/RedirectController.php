a335579deedac27ff9b0920d8f8bde24f0b299bd
Felix Jacobi <felix.jacobi@iserv.eu>
2018-03-02 16:25:23 +0100

    Added workaround for IServ MDM not supporting IPv6 yet

---
+++ src/Stsbl/IPv6Bundle/Controller/RedirectController.php
@@ -0,0 +1,46 @@
+<?php
+// src/Stsbl/IPv6Bundle/Controller/RedirectController.php
+namespace Stsbl\IPv6Bundle\Controller;
+
+use IServ\CoreBundle\Controller\PageController;
+use IServ\CoreBundle\Service\Config;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+class RedirectController extends PageController
+{
+    /**
+     * @var Config
+     */
+    private $config;
+
+    /**
+     * Action to redirect mdm request to IPv4 host
+     *
+     * @param Request $request
+     * @return RedirectResponse
+     */
+    public function redirectMdmAction(Request $request)
+    {
+        $newUrl = sprintf('https://ipv4.%s/iserv%s', $this->getConfig()->get('Domain'), $request->getPathInfo());
+
+        return new RedirectResponse($newUrl);
+    }
+
+    /**
+     * @return Config
+     */
+    public function getConfig()
+    {
+        return $this->config;
+    }
+
+    /**
+     * @param Config $config
+     * @required
+     */
+    public function setConfig(Config $config)
+    {
+        $this->config = $config;
+    }
+}
