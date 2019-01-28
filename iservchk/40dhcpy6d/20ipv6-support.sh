a5d0a86dc1f64978415103b0c8368ea5808fc378
Felix Jacobi <Felix@jacobi-hamburg.net>
2019-01-28 05:21:33 +0100

    dhcpy6d: Added workaround for multiple instances
    see: https://github.com/HenriWahl/dhcpy6d/issues/20

--- iservchk/40dhcpy6d/20ipv6-support.sh
+++ iservchk/40dhcpy6d/20ipv6-support.sh
@@ -9,6 +9,13 @@ then
   echo '  ---'
   echo '  dhcpy6d --generate-duid > /var/lib/iserv/ipv6-support/duid'
   echo
+  echo '# There seems to be multiple instances of dhcpy6d sometimes leading to'
+  echo '# log spam (see https://github.com/HenriWahl/dhcpy6d/issues/20)'
+  echo 'Test "single instance running"'
+  echo '  [ "$(pgrep -c dhcpy6d)" -lt 2 ]'
+  echo '  ---'
+  echo '  service stop dhcpy6d'
+  echo
   echo 'Check /etc/default/dhcpy6d'
   echo 'Check /etc/dhcpy6d.conf'
   echo 'Start dhcpy6d dhcpy6d'
