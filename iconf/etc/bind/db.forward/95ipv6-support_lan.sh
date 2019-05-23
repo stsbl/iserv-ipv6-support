808d1d537a428e02610694694fc50a9da0e9f770
Felix Jacobi <Felix@jacobi-hamburg.net>
2019-05-24 00:23:05 +0200

    Added native bind view support

--- iconf/etc/bind/db.forward/95ipv6-support_lan.sh
+++ iconf/etc/bind/db.forward/95ipv6-support_lan.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-. /usr/lib/iserv/cfg
-
-echo
-for i in $(netquery6 --global --lan --format ip; netquery6 --uniquelocal --format ip;)
+# Determine default IP or fallback
+DEFAULT_IP="$(netquery -p ip | head -1 | grep . || echo "127.0.0.1" )"
+# Determine IP on interface ICONF_SCHEMA_P0
+IP="$(netquery if ip | grep -E "^${ICONF_SCHEMA_P0:-invalid} " | awk '{print $2}' | head -n 1 | grep . || echo "$DEFAULT_IP" )"
+for i in $(netquery6 -gul -i "${ICONF_SCHEMA_P0:-invalid}" ip)
 do
   echo -e "@\t\tAAAA\t$i"
   echo -e "wpad\t\tAAAA\t$i"
@@ -11,9 +12,5 @@ do
   echo -e "ipv6\t\tAAAA\t$i"
 done
 
-for i in $(netquery -p ip)
-do
-  echo -e "ipv4\t\tA\t$i"
-done
-
+echo -e "ipv4\t\tA\t$IP"
 echo
--- iconf/etc/bind/db.iserv/95ipv6-support_lan.sh
+++ iconf/etc/bind/db.iserv/95ipv6-support_lan.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-. /usr/lib/iserv/cfg
-
-for i in $(netquery6 --global --lan --format ip; netquery6 --uniquelocal --format ip;)
+for i in $(netquery6 -gul -i "${ICONF_SCHEMA_P0:-invalid}" ip)
 do
   echo -e "@\t\tAAAA\t$i"
   echo -e "wpad\t\tAAAA\t$i"
