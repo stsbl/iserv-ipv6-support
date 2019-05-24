d23263e5e46e9398cd23b61c2c3a003adeb113e5
Felix Jacobi <Felix@jacobi-hamburg.net>
2019-05-24 20:44:43 +0200

    bind: Improved handling of default IP with LAN

--- iconf/etc/bind/db.forward/95ipv6-support_lan.sh
+++ iconf/etc/bind/db.forward/95ipv6-support_lan.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Determine default IP or fallback
-DEFAULT_IP="$(netquery -p ip | head -1 | grep . || echo "127.0.0.1" )"
-DEFAULT_IPv6=("$(netquery6 -g ip | head -n 1)" "$(netquery6 -u ip | head -n 1)")
+DEFAULT_IP="$(netquery -p ip | head -1 | grep . || echo "127.0.0.1")"
+DEFAULT_IPv6=("$(netquery6 -gl ip | head -n 1 | grep . || echo "::1")" "$(netquery6 -ul ip | head -n 1 | grep . || echo "::1")")
 # Determine IP on interface ICONF_SCHEMA_P0
 IPv4="$(netquery if ip | grep -E "^${ICONF_SCHEMA_P0:-invalid} " | awk '{print $2}' | head -n 1 | grep . || echo "$DEFAULT_IP" )"
 
-for i in $(netquery6 -gu -i "${ICONF_SCHEMA_P0:-invalid}" ip | grep . || echo "${DEFAULT_IPv6[@]}")
+for i in $(netquery6 -gu -i "${ICONF_SCHEMA_P0:-invalid}" ip | grep . || { echo "${DEFAULT_IPv6[@]}" | sort -u; })
 do
   echo -e "@\t\tAAAA\t$i"
   echo -e "wpad\t\tAAAA\t$i"
