056597e94643f376db0b5d7c05df84ab8b4c76e3
Felix Jacobi <Felix@jacobi-hamburg.net>
2019-05-25 01:01:01 +0200

    bind: Added updated default IP handling to legacy zone

--- iconf/etc/bind/db.iserv/95ipv6-support_lan.sh
+++ iconf/etc/bind/db.iserv/95ipv6-support_lan.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-for i in $(netquery6 -gul -i "${ICONF_SCHEMA_P0:-invalid}" ip)
+DEFAULT_IPv6=("$(netquery6 -gl ip | head -n 1 | grep . || echo "::1")" "$(netquery6 -ul ip | head -n 1 | grep . || echo "::1")")
+
+for i in $(netquery6 -gu -i "${ICONF_SCHEMA_P0:-invalid}" ip | grep . || { echo "${DEFAULT_IPv6[@]}" | sort -u; })
 do
   echo -e "@\t\tAAAA\t$i"
   echo -e "wpad\t\tAAAA\t$i"
