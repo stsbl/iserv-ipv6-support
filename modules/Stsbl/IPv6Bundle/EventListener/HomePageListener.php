stsbl/iserv-ipv6-support

commit be488c76c75fcd4c232b1b1080b5a2c42b918849

Fixed deprecations

modules/Stsbl/IPv6Bundle/EventListener/HomePageListener.php
@@ -1,10 +1,11 @@
-<?php declare(strict_types = 1);
+<?php
+declare(strict_types = 1);
 
 namespace Stsbl\IPv6Bundle\EventListener;
 
 use IServ\ComputerRequestBundle\Entity\ComputerRequest;
-use IServ\CoreBundle\Event\IDeskEvent;
-use IServ\CoreBundle\EventListener\IDeskListenerInterface;
+use IServ\CoreBundle\Event\HomePageEvent;
+use IServ\CoreBundle\EventListener\HomePageListenerInterface;
 use IServ\CoreBundle\Service\BundleDetector;
 use IServ\CoreBundle\Service\Config;
 use IServ\CoreBundle\Service\Shell;
@@ -15,8 +16,8 @@ use IServ\HostBundle\Util\Network;
 use Psr\Container\ContainerInterface;
 use Stsbl\IPv6Bundle\Util\Network6;
 use Symfony\Bridge\Doctrine\RegistryInterface;
-use Symfony\Component\DependencyInjection\ServiceSubscriberInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Contracts\Service\ServiceSubscriberInterface;
 
 /*
  * The MIT License
@@ -46,7 +47,7 @@ use Symfony\Component\HttpFoundation\RequestStack;
  * @author Felix Jacobi <felix.jacobi@stsbl.de>
  * @license MIT License <https://opensource.org/licenses/MIT>
  */
-class IDeskListener implements IDeskListenerInterface, ServiceSubscriberInterface
+class HomePageListener implements HomePageListenerInterface, ServiceSubscriberInterface
 {
 
     /**
@@ -91,7 +92,7 @@ class IDeskListener implements IDeskListenerInterface, ServiceSubscriberInterfac
      */
     private function generateSsoLink(): string
     {
-        $this->container->get('iserv.sudo');
+        $this->container->get(SudoService::class);
         $link = trim(Sudo::shell_exec('sudo /usr/lib/iserv/ipv6_generate_sso_link'));
 
         return $link;
@@ -115,7 +116,7 @@ class IDeskListener implements IDeskListenerInterface, ServiceSubscriberInterfac
         $this->shell = $shell;
     }
 
-    public function onBuildIDesk(IDeskEvent $event): void
+    public function onBuildHomePage(HomePageEvent $event): void
     {
         $ip = $this->request->getClientIp();
         $lan = $this->config->get('lan');

modules/Stsbl/IPv6Bundle/Controller/RedirectController.php
@@ -1,4 +1,5 @@
-<?php declare(strict_types = 1);
+<?php
+declare(strict_types = 1);
 
 namespace Stsbl\IPv6Bundle\Controller;
 
@@ -13,7 +14,7 @@ class RedirectController extends AbstractPageController
     /**
      * Action to redirect mdm request to IPv4 host
      */
-    public function redirectMdmAction(Request $request, Config $config): RedirectResponse
+    public function redirectMdm(Request $request, Config $config): RedirectResponse
     {
         $newUrl = sprintf('https://ipv4.%s/iserv%s', $config->get('Domain'), $request->getPathInfo());
 

modules/Stsbl/IPv6Bundle/DependencyInjection/StsblIPv6Extension.php
@@ -1,4 +1,5 @@
-<?php declare(strict_types = 1);
+<?php
+declare(strict_types = 1);
 
 namespace Stsbl\IPv6Bundle\DependencyInjection;
 

modules/Stsbl/IPv6Bundle/EventListener/KernelControllerSubscriber.php
@@ -1,10 +1,11 @@
-<?php declare(strict_types = 1);
+<?php
+declare(strict_types = 1);
 
 namespace Stsbl\IPv6Bundle\EventListener;
 
-use Doctrine\Common\Annotations\AnnotationException;
-use Doctrine\Common\Annotations\AnnotationReader;
+use Doctrine\Common\Annotations\Reader;
 use IServ\CoreBundle\Service\BundleDetector;
+use Stsbl\IPv6Bundle\Controller\RedirectController;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Controller\ControllerResolverInterface;
 use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
@@ -60,11 +61,17 @@ class KernelControllerSubscriber implements EventSubscriberInterface
      */
     private $bundleDetector;
 
-    public function __construct(BundleDetector $bundleDetector, ControllerResolverInterface $resolver, RouterInterface $router)
+    /**
+     * @var Reader
+     */
+    private $reader;
+
+    public function __construct(BundleDetector $bundleDetector, ControllerResolverInterface $resolver, RouterInterface $router, Reader $reader)
     {
         $this->bundleDetector = $bundleDetector;
         $this->resolver = $resolver;
         $this->router = $router;
+        $this->reader = $reader;
     }
 
     /**
@@ -121,13 +128,7 @@ class KernelControllerSubscriber implements EventSubscriberInterface
                 throw new \RuntimeException('Failed to reflect controller action!', 0, $e);
             }
 
-            try {
-                $annotationReader = new AnnotationReader();
-            } catch (AnnotationException $e) {
-                throw new \RuntimeException('Failed to create annotation reader!', 0, $e);
-            }
-
-            $annotations = $annotationReader->getMethodAnnotations($reflectionMethod);
+            $annotations = $this->reader->getMethodAnnotations($reflectionMethod);
             /* @var $annotation Route */
             list($annotation) = array_filter($annotations, function ($annotation) {
                 return $annotation instanceof Route;
@@ -149,7 +150,7 @@ class KernelControllerSubscriber implements EventSubscriberInterface
         }
 
         // duplicate original request
-        $request = $originalRequest->duplicate(null, null, ['_controller' => 'StsblIPv6Bundle:Redirect:redirectMdm']);
+        $request = $originalRequest->duplicate(null, null, ['_controller' => RedirectController::class . '::redirectMdm']);
         $controller = $this->resolver->getController($request);
 
         // skip unresolvable controller

modules/Stsbl/IPv6Bundle/StsblIPv6Bundle.php
@@ -1,4 +1,5 @@
-<?php declare(strict_types = 1);
+<?php
+declare(strict_types = 1);
 
 namespace Stsbl\IPv6Bundle;
 

modules/Stsbl/IPv6Bundle/Util/Network6.php
@@ -1,4 +1,5 @@
-<?php declare(strict_types = 1);
+<?php
+declare(strict_types = 1);
 
 namespace Stsbl\IPv6Bundle\Util;
 

commit 386ab073e629c86c5ccf2a271786f70a43008dc9

Fixed computer request dashboard (broken since revision be488c76)

modules/Stsbl/IPv6Bundle/EventListener/HomePageListener.php
@@ -47,7 +47,7 @@ use Symfony\Contracts\Service\ServiceSubscriberInterface;
  * @author Felix Jacobi <felix.jacobi@stsbl.de>
  * @license MIT License <https://opensource.org/licenses/MIT>
  */
-class HomePageListener implements HomePageListenerInterface, ServiceSubscriberInterface
+final class HomePageListener implements HomePageListenerInterface, ServiceSubscriberInterface
 {
 
     /**
@@ -162,7 +162,7 @@ class HomePageListener implements HomePageListenerInterface, ServiceSubscriberIn
     public static function getSubscribedServices(): array
     {
         return [
-            'iserv.sudo' => SudoService::class, // fetch sudo lazily to prevent eager util initialization!
+            SudoService::class, // fetch sudo lazily to prevent eager util initialization!
         ];
     }
 }