db87a1b219a3fcd39eea6aa4ca20b73eb7be62b7
Felix Jacobi <Felix@jacobi-hamburg.net>
2019-09-29 17:19:36 +0200

    Bumped copyright

--- modules/Stsbl/IPv6Bundle/EventListener/KernelControllerSubscriber.php
+++ modules/Stsbl/IPv6Bundle/EventListener/KernelControllerSubscriber.php
@@ -19,7 +19,7 @@ use Symfony\Component\Routing\RouterInterface;
 /*
  * The MIT License
  *
- * Copyright 2018 Felix Jacobi.
+ * Copyright 2019 Felix Jacobi.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
